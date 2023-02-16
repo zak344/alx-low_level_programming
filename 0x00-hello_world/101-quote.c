706179509a66eeddfca4b23b16d1cffddf15393d
zak344 <ayalnewy@gmail.com>
2023-02-17 08:46:15 +1300

    commited 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,13 @@ int main(void)
 {
 write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (1);
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
 }
 
