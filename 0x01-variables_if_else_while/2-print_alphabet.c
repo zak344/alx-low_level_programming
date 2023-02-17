e6bf4a8c255ea339fa2cfdf1b938b1664677ebb8
zak344 <ayalnewy@gmail.com>
2023-02-18 07:57:26 +1300

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* main - print out the alphbet */
+
+/* Return: 0 */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
