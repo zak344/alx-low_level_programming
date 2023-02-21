59110f6507844bf20221fb7742060aad2e0b8fb2
zak344 <ayalnewy@gmail.com>
2023-02-21 18:01:13 +1300

    update3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- *main - prints _putchar
+ * main - prints _putchar to stdout
  *
- *return: 0
+ * Return: 0
  */
+
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
