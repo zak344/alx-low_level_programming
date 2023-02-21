78a1f1c686d941e59b29b2747b03765daf5f7554
zak344 <ayalnewy@gmail.com>
2023-02-21 18:32:41 +1300

    nested loops and function1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if arg is lowercase
+ * @c: int to be checked
+ *
+ * Return: 1 if lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of arg
+ * @c: int to be evaluated
+ *
+ * Return: last digit of arg
+ */
+
+int print_last_digit(int c)
+{
+	int n;
+
+	n = c % 10;
+
+	if (n < 0)
+		n *= -1;
+	_putchar('0' + n);
+	return (n);
+}
+
