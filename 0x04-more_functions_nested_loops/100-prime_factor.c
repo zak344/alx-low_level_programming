cf02503a4a5acee8a23f84ac3b1b9526a99f199c
zak344 <ayalnewy@gmail.com>
2023-02-23 11:35:09 +1300

    more func and nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - compute 612852475143's largest prime number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%d\n", largestprime(612852475143, 2));
+	return (0);
+}
+
+/**
+ * largestprime - return largest prime factor of n
+ * @n: number to be factored
+ * @m: always 2 for smallest possible factor
+ *
+ * Return: largest prime factor of n
+ */
+
+int largestprime(long n, int m)
+{
+	while (n % m != 0)
+		m++;
+	if (m == n)
+		return (m);
+	else
+		return (largestprime(n / m, m));
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_number - prints out the specified number one char at a time
+ * @n: Number to be printed
+ * Description: zeroflag is set to print leading zeros when dropped from math.
+ */
+
+void print_number(int n)
+{
+	int size;
+
+	size = 1000000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	while (size > 0)
+	{
+		if (n >= size)
+		{
+			if (size >= 10)
+			{
+				_putchar((n / size) % 10 + '0');
+			}
+			else
+			{
+				_putchar(n % 10 + '0');
+				n = 0;
+			}
+		}
+		if (size > 1)
+			size = size / 10;
+		else
+			size = 0;
+	}
+}
