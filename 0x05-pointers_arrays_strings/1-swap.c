458d46d0f7d35359f6682968b250c41d5f5315de
zak344 <ayalnewy@gmail.com>
2023-02-27 17:13:52 +1300

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -11,7 +11,7 @@ void swap_int(int *a, int *b)
 {
 	*a = 98;
 	*b = 42;
-	printf("*a=%d, *b=%d\n", *a, *b);
+	printf(*a, *b);
 	swap_int(&*a, &*b);
-	printf("*a=%d, *b=%d\n", *a, *b);
+	printf(*a, *b);
 }
