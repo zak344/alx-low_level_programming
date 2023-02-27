1e167e977a25e54f6ae408a77585dbb2ac6452e2
zak344 <ayalnewy@gmail.com>
2023-02-27 18:12:22 +1300

    pointers,arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - print only even indexed chars from arg
+ * @str: string to print from
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
