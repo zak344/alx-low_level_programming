2b6638bf22834e5d3a2d82c69c6880e32b1d2c4c
zak344 <ayalnewy@gmail.com>
2023-03-02 07:36:14 +1300

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,34 +14,38 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (n1[i] != '\0')
 	{
-	i++;
+		i++;
 	}
 	while (n2[j] != '\0')
 	{
-	j++;
+		j++;
 	}
 	for (n = 0; n < j || n < i; n++)
 	{
-	a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
-	b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
-	c = a + b + dec;
-	r[n] = (c % 10) + '0';
-	dec = c > 9 ? 1 : 0;
+		a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
+		b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
+		c = a + b + dec;
+		r[n] = (c % 10) + '0';
+		dec = c > 9 ? 1 : 0;
 	}
 	if (dec == 1)
-	{ r[n] = '1';
-	r[n + 1] = '\0'; }
+	{
+	       	r[n] = '1';
+		r[n + 1] = '\0';
+       	}
 	else
-	{ r[n] = '\0';
-	n--; }
+	{
+	       	r[n] = '\0';
+		n--;
+       	}
 	for (i = 0; i < n + 1; i++)
 	{
-	for (j = 0; j < (n - i); j++)
-	{
-	aux = r[j + 1];
-	r[j + 1] = r[j];
-	r[j] = aux;
+		for (j = 0; j < (n - i); j++)
+		{
+			aux = r[j + 1];
+			r[j + 1] = r[j];
+			r[j] = aux;
+		}
 	}
-	}
-return (n < size_r - 1 ? r : 0);
+	return (n < size_r - 1 ? r : 0);
 }
