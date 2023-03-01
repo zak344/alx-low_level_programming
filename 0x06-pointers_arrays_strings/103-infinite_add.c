430c13c0bbe8bef267053f85196c6406479b380e
zak344 <ayalnewy@gmail.com>
2023-03-02 07:54:44 +1300

    advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,68 @@
 #include "main.h"
 
 /**
-  * infinite_add - adds two numbers
-  * @n1: number1
-  * @n2: number2
-  * @r: result
-  * @size_r: size result
-  * Return: r addition
-**/
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
+ *
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, a, b, c, n, aux, dec = 0;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	while (n1[i] != '\0')
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
+	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
 		i++;
 	}
-	while (n2[j] != '\0')
+	i = 0;
+	while (i < tmpl)
 	{
-		j++;
-	}
-	for (n = 0; n < j || n < i; n++)
-	{
-		a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
-		b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
-		c = a + b + dec;
-		r[n] = (c % 10) + '0';
-		dec = c > 9 ? 1 : 0;
-	}
-	if (dec == 1)
-	{
-	       	r[n] = '1';
-		r[n + 1] = '\0';
-       	}
-	else
-	{
-	       	r[n] = '\0';
-		n--;
-       	}
-	for (i = 0; i < n + 1; i++)
-	{
-		for (j = 0; j < (n - i); j++)
+		if (r[i] == '\0')
 		{
-			aux = r[j + 1];
-			r[j + 1] = r[j];
-			r[j] = aux;
+			break;
 		}
+		r[i] = tmp[i];
+		i++;
 	}
-	return (n < size_r - 1 ? r : 0);
+	return (r);
 }
