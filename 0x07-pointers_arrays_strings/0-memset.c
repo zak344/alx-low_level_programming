1ac046ed178d1b31c7fb29ba4bc6469731dee0a8
zak344 <ayalnewy@gmail.com>
2023-03-06 17:00:49 +1300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,16 @@
  * @s: Pointer to begin writting to memory
  * @b: Byte to fill memory with
  * @n: Number of Bytes to write
+ *
  * Return: Pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		*(s + i) = b;
+
 	return (s);
 }
