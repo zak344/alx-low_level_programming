50d93672999c5372ab4b16ee25a769b127d94e07
zak344 <ayalnewy@gmail.com>
2023-03-08 15:24:05 +1300

    recursion r

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_moreparams - Non-static resursive helper for finding sqrt
+ * @n: number to find square
+ * @c: numbers we have checked
+ *
+ * Return: The Square Root if rational, or -1
+ */
+int _sqrt_moreparams(int n, int c)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+
+	if (c * c > n)
+		return (-1);
+	else if (c * c == n)
+		return (c);
+	else
+		return (_sqrt_moreparams(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - find if a number is a natual square root
+ * @n: number to evaluate
+ *
+ * Return: The Square root, or -1
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_moreparams(n, 1));
+}
