efa3deb51f282f00dff6a09a8984390e120256de
zak344 <ayalnewy@gmail.com>
2023-04-04 11:18:44 +1200

    more singly linked lists 1

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,48 @@
+#ifndef LINKEDLISTS012_H
+#define LINKEDLISTS012_H
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+/**
+ * struct listint_safe - singly linked list
+ * @addy: address of this sister node
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_safe
+{
+	const listint_t *addy;
+	struct listint_safe *next;
+} listint_safe;
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint2(listint_t **head);
+void free_listint(listint_t *head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t
+*insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+#endif
