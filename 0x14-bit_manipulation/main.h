6382ef9de9f7280bb2ce569ee052caa7eb8c3677
zak344 <ayalnewy@gmail.com>
2023-04-06 09:02:09 +1200

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #define LONGINTBITS (sizeof(long int) * 8 - 1)
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
